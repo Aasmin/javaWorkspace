bc9a1c4b9dfe66a890a4d1660af97a90afaf43cf
Aasminpreet S Kainth <aasminpreetskainth@Aasminpreets-MacBook-Pro.local>
2020-05-07 21:32:59 -0400

    update

--- recursion/recursion02.java
+++ recursion/recursion02.java
@@ -1,5 +1,3 @@
-package recursion;
-
 public class recursion02 {
    
     public static void main(String[] args){
@@ -8,21 +6,24 @@ public class recursion02 {
 
     static void coinChange() {
         int[] coins = {2, 3, 5, 7};
-        System.out.println(permutations_INF(coins, 10, ""));
+        System.out.println(permutation_INF(coins, 10, ""));
     }
 
-    static int permutations_INF(int[] arr, int tar, String ans) {
-        if(tar == 0){
+    static int permutation_INF(int[] coins, int tar, String ans) {
+        // if(tar < 0) return 0;
+        if(tar == 0) {
             System.out.println(ans);
             return 1;
-        }
+        } 
+        
 
         int count = 0;
-        for(int coin : arr) {
-            if(tar - coin >= 0){
-            count += permutations_INF(arr, tar - coin, ans + " " + coin);
-            }
+        for(int i = 0; i < coins.length; i++) {
+            int coin = coins[i];
+            // if (tar - coin >= 0)
+                count += permutation_INF(coins, tar - coin, ans + coin + " ");
         }
+
         return count;
     }
 
