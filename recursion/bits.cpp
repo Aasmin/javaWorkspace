14dee9e198bb68482dd83dac8ebea35f8d1f3b8b
Aasmin <aasmin.kainth@gmail.com>
2020-05-11 00:58:09 -0400

    playing with bits

--- recursion/bits.cpp
+++ recursion/bits.cpp
@@ -2,9 +2,17 @@
 #include <vector>
 using namespace std;
 
-int offToOn(int num) {
-
+int offToOn(int num, int k) {
+    int mask = (1 << k);
+    return (num | mask);
 }
-int onToOff(int num) {
-    
+int onToOff(int num, int k) {
+    int mask = (~(1 << k));     //1s complement
+    return (num & mask);
+}
+
+int main() {
+    cout << offToOn(10, 0);
+    // cout << onToOff(5, 2);
+    return 0;
 }
