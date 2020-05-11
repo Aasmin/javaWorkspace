8335199a83874e4caa52b4c38f4c165300ccc567
Aasmin <aasmin.kainth@gmail.com>
2020-05-11 01:12:48 -0400

    tried another bits operation questions

--- recursion/bits.cpp
+++ recursion/bits.cpp
@@ -6,13 +6,22 @@ int offToOn(int num, int k) {
     int mask = (1 << k);
     return (num | mask);
 }
-int onToOff(int num, int k) {
+int onToOff01(int num, int k) {
     int mask = (~(1 << k));     //1s complement
     return (num & mask);
 }
 
+int onToOff02(int num, int k) {
+    int mask = (1 << k);     
+    if((num & mask) == 0) return num;
+    else return (num ^ mask);
+}
+
+
 int main() {
-    cout << offToOn(10, 0);
-    // cout << onToOff(5, 2);
+    // cout << offToOn(10, 0);
+    // cout << onToOff01(5, 2);
+    // cout << onToOff02(5, 2);
+    cout << (onToOff01(5, 2) == onToOff02(5, 2));
     return 0;
 }
