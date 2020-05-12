b890cf3e4884abe7a165e9614c3fc52822fd581f
Aasmin <aasmin.kainth@gmail.com>
2020-05-12 15:22:05 -0400

    median priority queue implementation

--- hashmap/mpq.java
+++ hashmap/mpq.java
@@ -1,25 +1,67 @@
 import java.util.*;
 
-public class mpq{ 
+class medianPQ {
 
-    static void add() {
+    // left is maximum priority Queue
+    private PriorityQueue<Integer> left = new PriorityQueue<>(Collections.reverseOrder());
+    // right is minimum Priority Queue
+    private PriorityQueue<Integer> right = new PriorityQueue<>();
 
+    private void handleBalance() {
+        if (left.size() - right.size() == 2) {
+            int rem = left.remove();
+            right.add(rem);
+        } else if (right.size() - left.size() == 2) {
+            int rem = right.remove();
+            left.add(rem);
+        }
     }
-    
-    static void remove() {
 
+    public void add(int val) {
+        if (left.size() > 0 && left.peek() > val) {
+            left.add(val);
+        } else {
+            right.add(val);
+        }
+
+        handleBalance();
     }
 
-    static int peek() {
-
+    public int remove() {
+        if (left.size() >= right.size()) {
+            return left.remove();
+        } else {
+            return right.remove();
+        }
     }
 
-    static int size() {
-
+    public int peek() {
+        if (left.size() >= right.size()) {
+            return left.peek();
+        } else {
+            return right.peek();
+        }
     }
 
+    public int size() {
+        return left.size() + right.size();
+    }
+}
 
+public class mPQ {
     public static void main(String[] args) {
-        
+        medianPQ pq = new medianPQ();
+        pq.add(10);
+        pq.add(20);
+        pq.add(5);
+        pq.add(19);
+        System.out.println(pq.peek());
+        pq.add(50);
+        System.out.println(pq.peek());
+        System.out.println(pq.remove());
+        System.out.println(pq.peek());
+        pq.add(3);
+        pq.add(2);
+        System.out.println(pq.peek());
     }
 }
