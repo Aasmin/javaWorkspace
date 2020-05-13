c2879dc77279e7e086fc606c4630b1044fb9d32a
Aasmin <aasmin.kainth@gmail.com>
2020-05-12 23:48:41 -0400

    singleNumberii

--- recursion/questions.cpp
+++ recursion/questions.cpp
@@ -95,8 +95,24 @@ vector<int> out;    int val = 0;
     return out;
 }
 
+//leetcode 137
+ int singleNumberii(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+        int count = 0;
+        int mask = (1 << i);
+        for(int ele : nums)     
+            if((ele & mask) != 0)
+                count++;
+        if(count % 3 != 0) {
+            ans |= mask;
+            }
+        }
+        return ans;
+    }
+
 //leetcode 260
-    vector<int> singleNumber(vector<int>& nums) {
+    vector<int> singleNumberiii(vector<int>& nums) {
         int xorNum = 0;
         for(int ele : nums)
             xorNum ^= ele;
