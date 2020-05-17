5e54913a26bbacac86811b7bacffc42709255f32
Aasmin <aasmin.kainth@gmail.com>
2020-05-17 10:07:57 -0400

    equiset1 solved

--- recursion/l003.java
+++ recursion/l003.java
@@ -1,10 +1,8 @@
 import java.util.ArrayList;
 public class l003 {
-
-
-    
 //EquiSet Problem
-public static int equeSet(int[] arr, int sum1, int sum2, int idx, String set1, String set2) {
+public static int equeSet(int[] arr, int idx, int sum1, int sum2,String set1, String set2) {
+    
     if(idx == arr.length) {
         if(sum1 == sum2) {
             System.out.println(set1 + "== " + set2);
@@ -13,16 +11,34 @@ public static int equeSet(int[] arr, int sum1, int sum2, int idx, String set1, S
         return 0;
     }
     int count = 0;
-    count += equeSet(arr, sum1 + arr[idx], sum2, idx + 1, set1 + arr[idx] + " " , set2);
-    count += equeSet(arr, sum1, sum2 + arr[idx], idx + 1, set1, set2 + arr[idx] + " ");
-    count += equeSet(arr, sum1, sum2, idx + 1, set1, set2);
-  
+    count += equeSet(arr, idx + 1, sum1 + arr[idx], sum2, set1 + arr[idx] + " " , set2);
+    count += equeSet(arr, idx + 1, sum1, sum2 + arr[idx], set1, set2 + arr[idx] + " ");
     return count;
     }
 
+    public static int equeSet2(int[] arr, int idx, int sum1, int sum2,String set1, String set2) {
+    
+        if(idx == arr.length || sum1 == sum2) {
+            if(sum1 == sum2) {
+                System.out.println(set1 + "== " + set2);
+                return 1;
+            }
+            return 0;
+        }
+        int count = 0;
+        count += equeSet2(arr, idx + 1, sum1 + arr[idx], sum2, set1 + arr[idx] + " " , set2);
+        count += equeSet2(arr, idx + 1, sum1, sum2 + arr[idx], set1, set2 + arr[idx] + " ");
+        return count;
+        }
+    
+        static void setProblem() {
+            int[] arr = {10, 20, 30, 40, 50, 60, 70, 80};
+            System.out.println(equeSet(arr, 0, 0, 0, "", ""));
+            // System.out.println(equeSet2(arr, 0, 0, 0, "", ""));
+    
+        }
+    
     public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-        System.out.println(equeSet(arr, 0, 0, 0, "", ""));
-        // System.out.println(equiSet(arr, 0, 0, 0, "", ""));
+        setProblem();
     }
 }
