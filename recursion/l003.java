42353ba274da5669b8d4d32817428c33061a4b19
Aasmin <aasmin.kainth@gmail.com>
2020-05-17 10:35:28 -0400

    equiset2 solved

--- recursion/l003.java
+++ recursion/l003.java
@@ -17,24 +17,28 @@ public static int equeSet(int[] arr, int idx, int sum1, int sum2,String set1, St
     }
 
     public static int equeSet2(int[] arr, int idx, int sum1, int sum2,String set1, String set2) {
-    
-        if(idx == arr.length || sum1 == sum2) {
-            if(sum1 == sum2) {
-                System.out.println(set1 + "== " + set2);
-                return 1;
-            }
-            return 0;
-        }
         int count = 0;
+        
+        if(sum1 == sum2 && sum1 != 0) {
+            System.out.println(set1 + "== " + set2);
+            count++;
+        }
+        
+        if(idx == arr.length) { 
+            
+            return count;
+        }
         count += equeSet2(arr, idx + 1, sum1 + arr[idx], sum2, set1 + arr[idx] + " " , set2);
         count += equeSet2(arr, idx + 1, sum1, sum2 + arr[idx], set1, set2 + arr[idx] + " ");
+        count += equeSet2(arr, idx + 1, sum1, sum2, set1, set2);
+
         return count;
         }
     
         static void setProblem() {
-            int[] arr = {10, 20, 30, 40, 50, 60, 70, 80};
-            System.out.println(equeSet(arr, 0, 0, 0, "", ""));
-            // System.out.println(equeSet2(arr, 0, 0, 0, "", ""));
+            int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+            // System.out.println(equeSet(arr, 0, 0, 0, "", ""));
+            System.out.println(equeSet2(arr, 0, 0, 0, "", ""));
     
         }
     
