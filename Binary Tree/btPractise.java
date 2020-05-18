c333f64765df97b8a1f933a106aa5504484db375
Aasmin <aasmin.kainth@gmail.com>
2020-05-18 16:44:49 -0400

    Binary Tree construction

---
+++ Binary Tree/btPractise.java
@@ -0,0 +1,17 @@
+public class btPractise {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
