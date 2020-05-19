c215fef87997e8993965c3ca685289ce0cfea37a
Aasmin <aasmin.kainth@gmail.com>
2020-05-18 23:59:27 -0400

    BT levelOrder traversal

--- Binary Tree/btPractise.java
+++ Binary Tree/btPractise.java
@@ -1,4 +1,6 @@
 import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Queue;
 public class btPractise {
     public static class Node {
         int data;
@@ -103,6 +105,23 @@ public class btPractise {
         traversals(node.right);
         System.out.println(node.data + " in POST order");    //Euler right
     }
+
+    public static void levelOrder(Node node) {
+        Queue<Node> mq = new ArrayDeque<>();
+        mq.add(node);
+
+        while(mq.size() != 0) {
+            int count = mq.size();
+            for(int i = 0; i < count; i++) {
+                node = mq.remove();
+                System.out.println(node.data);
+
+                if(node.left != null)   mq.add(node.left);
+                if(node.right != null)   mq.add(node.right);
+            }
+        }
+
+    }
     public static void main(String[] args) {
         Integer[] arr = {50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null, null};
         Node root = construct(arr);
@@ -111,6 +130,7 @@ public class btPractise {
         // System.out.println(sum(root));
         // System.out.println(height(root));   
         // System.out.println(max(root));
-        traversals(root);
+        // traversals(root);
+        levelOrder(root);
     }
 }
