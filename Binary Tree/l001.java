db342fd19144e752e2b1d2165e4d2350ca645679
Aasmin <aasmin.kainth@gmail.com>
2020-05-24 22:00:22 -0400

    BT  Random Learning

--- Binary Tree/l001.java
+++ Binary Tree/l001.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 public class l001{
 
     public static void main(String[] args){
@@ -86,6 +86,7 @@ public class l001{
        if(node.data==data) return true;
        return find(node.left,data) || find(node.right,data);   
         
+
         // if(find(node.left,data)) return true;
         // if(find(node.rigth,data)) return true;
   
@@ -188,112 +189,344 @@ public class l001{
         return prev;
     } 
 
-    static Node LCANode = null;
-    public static boolean lowestCommonAncestor_(Node node, int p, int q) {
-        if(node == null) return false;
+    static Node LCANode=null;
+    public static boolean lowestCommonAncestor_02(Node root, int p, int q) {
+        if(root==null) return false;
+        
+        boolean selfDone=false;
+        if(root.data==p || root.data==q){
+            selfDone=true;
+        }
 
-        boolean selfdone = false;
-        if(node.data == p || node.data == q)        selfdone = true;
+        boolean leftDone=lowestCommonAncestor_02(root.left,p,q);
+        if(LCANode!=null) return true;
 
-        boolean lnode = lowestCommonAncestor_(node.left, p, q);
-        if(LCANode != null) return true;
+        boolean rightDone=lowestCommonAncestor_02(root.right,p,q);
+        if(LCANode!=null) return true;
+        
+        if((selfDone && leftDone) || (selfDone && rightDone) || (leftDone && rightDone))
+          LCANode=root;
 
-        boolean rnode = lowestCommonAncestor_(node.right, p, q);
-        if(LCANode != null) return true;
 
-        if((lnode && rnode) || (lnode && selfdone) || (rnode && selfdone))
-            LCANode = node;
-
-        return selfdone || lnode || rnode;
+        return selfDone || leftDone || rightDone; 
     }
 
-   
-    public List<Integer> distanceK(Node root, Node target, int K) {
-        ArrayList<Node> path = new ArrayList<>();
-        nodeToRootPath(root, target.data, path);
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i < path.size(); i++) {
-            if(i == 0)
-                kDown(path.get(i), K - i, null, ans);
-            else
-                kDown(path.get(i), K - i, path.get(i - 1), ans);
+    public static void kDown(Node root,int level,Node blockNode){
+       if(root==null || root==blockNode) return;
+
+       if(level==0){
+         System.out.print(root.data + " ");
+         return;  
+       }
+
+       kDown(root.left,level-1,blockNode);
+       kDown(root.right,level-1,blockNode);
+
+    }
+
+    public static void allNodeKAway(Node root, int target, int K) {
+        ArrayList<Node> path=new ArrayList<>();
+        rootToNodePath_(root,target,path);
+
+        Node blockNode=null;
+        for(int i=0;i<path.size();i++){
+            if(K-i<0) break;
+            kDown(path.get(i),K-i,blockNode);
+            blockNode=path.get(i);
         }
-        return ans;
     }
     
-    public void kDown(Node node, int K, Node block, List<Integer> ans) {
-        if(K < 0 || node == null || node == block)   return;
-        if(K == 0)  ans.add(node.data);
-        kDown(node.left, K - 1, block, ans);
-        kDown(node.left, K - 1, block, ans);
-    }
-    
-    public boolean nodeToRootPath(Node node, int target, ArrayList<Node> path) {
-        if(node == null)    return false;
-        
-        if(node.data == target) {
-            path.add(node);
-            return true;
-        }
-        
-        boolean filc = nodeToRootPath(node.left, target, path);
-        if(filc){
-            path.add(node);
-            return true;
-        }
-        
-        boolean firc = nodeToRootPath(node.right, target, path);
-        if(firc){
-            path.add(node);
-            return true;
-        }
+    public static int allNodeKAway_02_(Node root, int target, int K) {
+         if(root==null) return -1;
 
-        return false;
-    }
-
-
-    // 21 may
-    public static void kDown(Node node, int K) {
-        if(K < 0 || node == null)   return;
-        if(K == 0) System.out.println(node.data);;
-        kDown(node.left, K - 1);
-        kDown(node.right, K - 1);
-    }
-
-    public static int nodeToRootPath_03(Node node, int target, int k) {
-        if(node.data == target) {
-            kDown(node, k);
+         if(root.data == target){
+            kDown(root,K,null);
             return 1;
+         }
+
+         int leftdistance=allNodeKAway_02_(root.left,target,K);
+         if(leftdistance!=-1){
+            if(K-leftdistance >= 0) kDown(root,K-leftdistance,root.left);
+            return leftdistance+1;
+         }
+         
+         int rightdistance=allNodeKAway_02_(root.right,target,K);
+          if(rightdistance!=-1){
+            if(K-rightdistance >= 0) kDown(root,K-rightdistance,root.right);
+            return rightdistance+1;
         }
 
-        int leftDistance = nodeToRootPath_03(node.left, target, k);
-        if(leftDistance != -1) {
-            if(k - leftDistance == 0) System.out.println(node.data + " ");
-            else kDown(node.right, k - leftDistance - 1);
-            return leftDistance + 1;
-        }
-
-        int rightDistance = nodeToRootPath_03(node.right, target, k);
-        if(rightDistance != -1) {
-            if(k - rightDistance == 0) System.out.println(node.data + " ");
-            else kDown(node.left, k - rightDistance - 1);
-            return leftDistance + 1;
-        }
-
-        return 0;
+        return -1;
+    
     }
+
+    public static void kDown(Node root,int level){
+        if(root==null) return;
+ 
+        if(level==0){
+          System.out.print(root.data + " ");
+          return;  
+        }
+ 
+        kDown(root.left,level-1);
+        kDown(root.right,level-1);
+ 
+     }
+
+    public static int allNodeKAway_03_(Node root, int target, int K) {
+        if(root==null) return -1;
+
+        if(root.data == target){
+           kDown(root,K,null);
+           return 1;
+        }
+
+        int leftdistance=allNodeKAway_03_(root.left,target,K);
+        if(leftdistance!=-1){
+           if(K-leftdistance == 0)  
+              System.out.print(root.data + " ");
+           else
+              kDown(root.right,K-leftdistance-1);
+           return leftdistance+1;
+        }
+        
+        int rightdistance=allNodeKAway_03_(root.right,target,K);
+        if(rightdistance!=-1){
+            if(K-rightdistance == 0)
+               System.out.print(root.data + " ");
+            else
+               kDown(root.left,K-rightdistance-1);
+            return rightdistance+1;
+         }
+
+       return -1;
+   
+   }
+
+   public static int diameter_01(Node node){
+       if(node==null) return 0;
+
+       int ld=diameter_01(node.left);
+       int rd=diameter_01(node.right);
+
+       int lh=height(node.left);
+       int rh=height(node.right);
+
+       int myDia = lh + rh + 2;
+       return Math.max(Math.max(ld,rd), myDia);
+   }
+
+   public static class diaPair{
+       int dia=0;
+       int hei=0;
+
+       diaPair(int dia,int hei){
+           this.dia=dia;
+           this.hei=hei;
+       }
+   }
+
+   public static diaPair diameter_02(Node node){
+    if(node==null) return new diaPair(0,-1) ;
+
+    diaPair lr=diameter_02(node.left); // left result
+    diaPair rr=diameter_02(node.right); // right result
+
+    diaPair myRes=new diaPair(0,-1);
+    myRes.dia = Math.max(Math.max(lr.dia,rr.dia), (lr.hei+rr.hei+2));
+    myRes.hei = Math.max(lr.hei,rr.hei)+1;
+    
+    return myRes;
+}
+
+static int diameter=0;
+public static int diameter_03(Node node){
+    if(node==null) return -1 ;
+
+    int lh = diameter_03(node.left); // left height
+    int rh =diameter_03(node.right); // right height
+    
+    diameter=Math.max(diameter,lh+rh+2);
+    return Math.max(lh,rh)+1;
+}
+
+   //LevelOrder_series.=========================================================
+
+   public static void levelOrder_00(Node node){
+       LinkedList<Node> pQue=new LinkedList<>(); // addLast and removeFirst.
+       pQue.addLast(node);
+
+       while(pQue.size()!=0){
+           Node rnode=pQue.removeFirst();
+           System.out.print(rnode.data+ " ");
+           if(rnode.left!=null) pQue.addLast(rnode.left);
+           if(rnode.right!=null) pQue.addLast(rnode.right);
+      }
+   }
+
+   public static void levelOrder_01(Node node){
+    LinkedList<Node> pQue=new LinkedList<>(); // addLast and removeFirst.
+    LinkedList<Node> cQue=new LinkedList<>(); // addLast and removeFirst.
+
+    pQue.addLast(node);
+    int count=0;
+    System.out.print("Level: " + count + " -> ");
+    
+    while(pQue.size()!=0){
+        Node rnode=pQue.removeFirst();
+        System.out.print(rnode.data+ " ");
+        if(rnode.left!=null) cQue.addLast(rnode.left);
+        if(rnode.right!=null) cQue.addLast(rnode.right);
+
+        if(pQue.size()==0){
+          LinkedList<Node> temp=pQue;
+          pQue=cQue;
+          cQue=temp;
+          count++;      
+         System.out.print("\nLevel: " + count + " -> ");
+        }
+    }
+}
+
+public static void levelOrder_02(Node node){
+    LinkedList<Node> Que=new LinkedList<>(); // addLast and removeFirst.
+
+    Que.addLast(node);
+    Que.addLast(null);
+    int count=0;
+    System.out.print("Level: " + count + " -> ");
+    
+    while(Que.size()!=1){
+        Node rnode=Que.removeFirst();
+        System.out.print(rnode.data+ " ");
+        if(rnode.left!=null) Que.addLast(rnode.left);
+        if(rnode.right!=null) Que.addLast(rnode.right);
+
+        if(Que.getFirst()==null){
+            Que.removeFirst();
+            Que.addLast(null);
+            count++;      
+            System.out.print("\nLevel: " + count + " -> ");
+        }
+    }
+}
+
+public static void levelOrder_03(Node node){
+    LinkedList<Node> pQue=new LinkedList<>(); // addLast and removeFirst.
+    pQue.addLast(node);
+    
+    int count=0;
+    while(pQue.size()!=0){
+        System.out.print("Level: " + count + " -> ");
+        count++;
+        int size=pQue.size();
+   
+        while(size-- > 0){
+            Node rnode=pQue.removeFirst();
+            System.out.print(rnode.data+ " ");
+            if(rnode.left!=null) pQue.addLast(rnode.left);
+            if(rnode.right!=null) pQue.addLast(rnode.right);    
+        }
+        System.out.println();
+    }
+}
+
+public static void leftView(Node node){
+    LinkedList<Node> que=new LinkedList<>(); // addLast and removeFirst.
+    que.addLast(node);
+    while(que.size()!=0){
+        int size=que.size();
+        System.out.print(que.getFirst().data + " ");
+        while(size-- > 0){
+            Node rnode=que.removeFirst();
+            if(rnode.left!=null) que.addLast(rnode.left);
+            if(rnode.right!=null) que.addLast(rnode.right);    
+        }
+    }
+}
+
+//swapped the left right child addition calls
+public static void rightView2(Node node){
+    LinkedList<Node> que=new LinkedList<>(); // addLast and removeFirst.
+    que.addLast(node);
+    while(que.size()!=0){
+        int size=que.size();
+        System.out.print(que.getFirst().data + " ");
+        while(size-- > 0){
+            Node rnode=que.removeFirst();
+            if(rnode.right!=null) que.addLast(rnode.right);    
+            if(rnode.left!=null) que.addLast(rnode.left);
+        }
+    }
+}
+
+public static void rightView(Node node){
+    LinkedList<Node> que=new LinkedList<>(); // addLast and removeFirst.
+    que.addLast(node);
+    while(que.size()!=0){
+        int size=que.size();
+        Node prev = null;
+        while(size-- > 0){
+            Node rnode=que.removeFirst();
+            if(rnode.left!=null) que.addLast(rnode.left);
+            if(rnode.right!=null) que.addLast(rnode.right); 
+            prev = rnode;   
+        }
+        System.out.print(prev.data + " ");
+    }
+}
+
+public static void newView(Node node) {
+    LinkedList<Node> que = new LinkedList<>();
+    que.add(node);
+    while(que.size() != 0) {
+        int size = que.size();
+        while(size-- > 0) {
+            Node rnode = que.removeFirst();
+            System.out.print(rnode.data + " ");
+            if(node.left != null)   que.addLast(node.left);
+            if(node.right != null)   que.addLast(node.right);
+        }
+        System.out.println();
+    }
+}
+
+
+   public static void levelOrder(Node node){
+    // levelOrder_00(node);
+    // levelOrder_01(node);
+    // levelOrder_02(node);
+    // levelOrder_03(node);
+    // leftView(node);
+    // System.out.println();
+    // rightView(node);
+    // System.out.println();
+    // rightView2(node);
+    newView(node);
+   }
+
 
    public static void set1(Node node){
     //   rootToNodePath(node,100);
-    nodeToRootPath_03(node, 100, 1);
+    
+    //   lowestCommonAncestor_02(node,200,30);
+    //   System.out.println("LCA: " + (LCANode!=null?LCANode.data:"-1"));
+    // allNodeKAway(node,20,1);
 
+
+    // System.out.println(diameter_01(node));
+    // System.out.println(diameter_02(node).dia);
+    // diameter_03(node);
+    // System.out.println(diameter);
    }
-   
+
    public static void solve(){
-       int[] arr={10,20,40,-1,-1,50,80,-1,-1,90,-1,-1,30,60,100,-1,-1,-1,70,110,-1,-1,120,-1,-1};
+    //    int[] arr={10,20,40,-1,-1,50,80,-1,-1,90,-1,-1,30,60,100,-1,-1,-1,70,110,-1,-1,120,-1,-1};
+    int[] arr = {11, 6, 4, -1, 5, -1, -1, 8, -1, 10, -1, -1, 19, 17, -1, -1, 43, 31, -1, -1, 49, -1, -1};
     //    int[] arr={10,20};
        Node root=constructTree(arr);
        display(root);
-       set1(root);
+    //    set1(root);
+       levelOrder(root);
    }
 }
