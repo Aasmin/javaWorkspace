b781966b574a7a5892c77a989b19a3a0199b225a
Aasmin <aasmin.kainth@gmail.com>
2020-06-03 14:23:31 -0400

    BST Pred and Succ inOrder logN

--- Binary Tree/l001.cpp
+++ Binary Tree/l001.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+//BST PRACTICE
 using namespace std;
 
 class Node
@@ -84,7 +84,7 @@ bool findRec (Node *node, int data) {   //O(log n)
     return res;
 }
 
-int maximumEle(Node *node) {
+int maximumEle(Node *node) {    //logn
     if(node == nullptr) return -1;
     while(node->right != nullptr) {
         node = node->right;
@@ -98,26 +98,115 @@ int minimumEle(Node *node) {
         node = node->left;
     return node->data;
 }
+int LCAoFBST_Rec(Node *node, int a, int b) // a<b
+{
+    if (node == nullptr)
+        return -1;
 
-Node *lca (Node node) {
-    
+    if (b < node->data && a < node->data)
+        return LCAoFBST_Rec(node->left, a, b);
+    else if (a > node->data && b > node->data)
+        return LCAoFBST_Rec(node->right, a, b);
+    else
+        return node->data; //LCA point.
 }
+
+int LCAoFBST(Node *node, int a, int b) // a<b
+{
+    Node *curr = node;
+    while (curr != nullptr)
+    {
+        if (b < curr->data && a < curr->data)
+            curr = curr->left;
+        else if (a > curr->data && b > curr->data)
+            curr = curr->right;
+        else
+            return find(curr, a) && find(curr, b) ? curr->data : -1; //LCA point.
+    }
+
+    return -1;
+}
+
+//In Order
+void allNodesInRange_01(Node *node, int a, int b, vector<int> &ans)
+{
+    if (node == nullptr)
+        return;
+
+    allNodesInRange_01(node->left, a, b, ans);
+
+    // sorted Region
+    if (node->data >= a && node->data <= b)
+        ans.push_back(node->data);
+    
+    allNodesInRange_01(node->right, a, b, ans);
+}
+
+//pre Order
+void allNodesInRange_02(Node *node, int a, int b, vector<int> &ans)
+{
+    if (node == nullptr)
+        return;
+    if (node->data >= a && node->data <= b)
+        ans.push_back(node->data);
+
+    if (b < node->data && a < node->data)
+        allNodesInRange_02(node->left, a, b, ans);
+    else if (a > node->data && b > node->data)
+        allNodesInRange_02(node->right, a, b, ans);
+    else //LCA Region.
+    {
+        allNodesInRange_02(node->left, a, b, ans);
+        allNodesInRange_02(node->right, a, b, ans);
+    }
+}
+
+void preAndSucc_InOrder(Node *node, int data) {
+    Node *curr = node;
+    Node *pred = nullptr;
+    Node *succ = nullptr;
+    while(curr != nullptr) {
+        if(curr->data == data) {
+            //Pred
+            if(curr->left == nullptr) {
+                cout << "Pred: " << (pred == nullptr? -1 : pred->data) << endl;
+            } else {
+                pred = curr->left;
+                while(pred->right != nullptr)
+                    pred = pred->right;
+                cout << "Pred: " << pred->data <<endl;
+            }
+
+            //Succ
+            if(curr->right == nullptr) {
+                cout << "Succ: " << (succ == nullptr? -1 : succ->data) << endl;
+            } else {
+                succ = curr->right;
+                while(succ->left != nullptr)
+                    succ = succ->left;
+                cout << "Succ: " << succ->data <<endl;
+            }
+            
+        } else if (data < curr->data) {
+            succ = curr;
+            curr = curr->left;
+        } else {
+            pred = curr;
+            curr = curr->right;
+        }
+    }
+}
+
 
 void solve()
 {
-    // vector<int> arr = {10, 20, 40, -1, -1, 50, 80, -1, -1, 90, -1, -1, 30, 60, 100, -1, -1, -1, 70, 110, -1, -1, 120, -1, -1};
     vector<int> arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130};
     Node *root = constructBST(arr, 0, arr.size() - 1);
     display(root);
-    // cout << height(root) << endl;
-    // cout << size(root) << endl;
-    cout << find(root, 20) << endl;
-    cout << findRec(root, 20) << endl;
-    // cout << maximumEle(root) << endl;
-    // cout << minimumEle(root) << endl;
 }
 
-int main(){
+int main()
+{
     solve();
     return 0;
 }
