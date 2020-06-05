d0452c6e5b67f022a9833a40144ec287aa4d5e8c
Aasmin <aasmin.kainth@gmail.com>
2020-06-05 13:58:18 -0400

    AVL Minor correction in updateHTandBal

--- Binary Tree/AVL.java
+++ Binary Tree/AVL.java
@@ -19,18 +19,18 @@ public class AVL {
     }
     
     //AVL Util.===========================================================================
-    public static void updateHeightAndBalance(Node node) {
+    public static void updateHeightAndBalance(Node node) {  // O(1)
         int lh = -1;
         int rh = -1;
 
         if(node.left != null)   lh = node.left.height;
-        if(node.right != null)   lh = node.right.height;
+        if(node.right != null)   rh = node.right.height;
         
         node.height = Math.max(lh, rh) + 1;
         node.balance = lh - rh;
     }
 
-    public static Node ll(Node A) {
+    public static Node ll(Node A) { // O(1)
         Node B = A.left;
         Node BkaRight = B.right;
 
@@ -42,7 +42,7 @@ public class AVL {
         return B;
     }
 
-    public static Node rr(Node A) {
+    public static Node rr(Node A) { // O(1)
         Node B = A.right;
         Node BkaLeft = B.left;
 
@@ -55,7 +55,7 @@ public class AVL {
         return B;
     }
 
-    public static Node getRotation(Node node) {
+    public static Node getRotation(Node node) { // O(1)
         updateHeightAndBalance(node);
         if(node.balance == 2) {  //ll, lr
             if(node.left.balance == 1) {    //ll
@@ -90,22 +90,22 @@ public class AVL {
         return node;
     }
 
-    public static void display(Node node)
+    public static void display(Node node)   // O(n)
     {
         if (node == null)
             return;
 
         String str = "";
-        str += ((node.left != null) ? (node.left.data) : ".");
-        str += " <- " + node.data + " . ";
-        str += ((node.right != null) ? (node.right.data) : ".");
-        System.out.println(str + "\n");
+        str += ((node.left != null) ? (node.left.data) + "[" + node.left.balance + "," + node.left.height + "]" : ".  \t\t");
+        str += " \t <-  \t" + node.data  + "[" + node.balance + "," + node.height + "]" + " \t ->  \t";
+        str += ((node.right != null) ? (node.right.data) + "[" + node.right.balance + "," + node.right.height + "]" : " \t\t.");
+        System.out.println(str);
 
         display(node.left);
         display(node.right);
     }
 
-    public static Node addNode(Node node, int data) {
+    public static Node addNode(Node node, int data) { // O(logn)
         if(node == null)     return new Node(data);
         
         if(data < node.data) 
@@ -117,7 +117,7 @@ public class AVL {
     }
 
 
-    int maximumEle(Node node) {    //logn
+    static int maximumEle(Node node) {    //logn
         if(node == null) return -1;
         while(node.right != null) {
             node = node.right;
@@ -126,7 +126,7 @@ public class AVL {
     }
 
 
-    Node removeData(Node node, int data) {
+    static Node removeData(Node node, int data) {  //logn
         if(node == null)     return null; //never found data
         
         if(data < node.data) 
@@ -145,12 +145,25 @@ public class AVL {
     }
     
     public static void solve() {
-    // int[] arr = {10, 20, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130};
-    // Node root = constructBST(arr, 0, arr.size() - 1);
+    // int[] arr1 = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130};
+    // Node root = constructBST(arr1, 0, arr1.length - 1);
+    // root = addNode(root, 55);
+    // root = addNode(root, 54);
+    // root = removeData(root, 54);
+    // root = removeData(root, 40);
 
+    int[] arr = {100, 40, 20, 70, 30, 80, 10, 60, 90, 50, 130, 120, 110};
+    // Node *root = constructBST(arr, 0, arr.size() - 1);
 
+    Node root = null;
+    for (int i = 0; i < arr.length; i++)
+    {
+        root = addNode(root, arr[i]);
+        display(root);
+        System.out.println("Total nodes added: " + (i + 1) + "\n\n\n");
+    }
 
-
+    // display(root);
     }
     public static void main(String[] args) {
         solve();
