40103ca1f2357108e060858169f94de9b8c73b1b
Aasmin <aasmin.kainth@gmail.com>
2020-06-13 16:07:06 -0400

    updates

--- LinkedList/questions.java
+++ LinkedList/questions.java
@@ -1,20 +1,25 @@
 public class questions {
     public class ListNode {
-            int val;
-            ListNode next;
-            ListNode() {}
-            ListNode(int val) { this.val = val; }
-            ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-        }
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
 
-        //Leetcode: 876. Middle of the Linked List(return the second middle node)
-        public ListNode middleNode(ListNode head) {
-            ListNode slow = head;
-            ListNode fast = head;
-            while(fast != null && fast.next != null){
-                fast = fast.next.next;
-                slow = slow.next;
-            }
-            return slow;
+    //Leetcode: 876. Middle of the Linked List(return the second middle node)
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
         }
+        return slow;
+    }
+
+    //Leetcode: 206. Reverse Linked List
+    public ListNode reverseList(ListNode head) {
+    
+    }
 }
