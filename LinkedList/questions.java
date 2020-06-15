d2441543b61060db8b5e621b06ddbffb60b1cdc4
Aasmin <aasmin.kainth@gmail.com>
2020-06-15 12:03:30 -0400

    LL Lc 142 Linked List Cycle II

--- LinkedList/questions.java
+++ LinkedList/questions.java
@@ -367,6 +367,27 @@ public class questions {
         return false;
     }
     
+    //Leetcode 142. Linked List Cycle II
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null)   return null;
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {              //Step 1. Find the first meeting point
+                slow = head;
+                while(slow != fast)   {     //Step 2. Find the intersection point by moving both ptrs at same speed;
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+
+        return null;
+    }
 
     public static void display(ListNode node) {
         if(node == null) return;
