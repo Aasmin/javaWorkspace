8f2284f1b0437256ef535954df7a1eeafa023e42
Aasmin <aasmin.kainth@gmail.com>
2020-06-16 00:36:51 -0400

    LL LC 25. Reverse Nodes in k-Group

--- LinkedList/questions.java
+++ LinkedList/questions.java
@@ -430,6 +430,76 @@ public class questions {
         return mergeTwoLists(mergeKLists(lists, si, mid), mergeKLists(lists, mid + 1, ei));
     }
 
+    //Leetcode 25. Reverse Nodes in k-Group
+    ListNode oh = null;
+    ListNode ot = null;
+
+    ListNode th = null;
+    ListNode tt = null;
+
+    public void addFirstNode(ListNode node) {
+        if (th == null) {
+            th = node;
+            tt = node;
+        } else {
+            node.next = th;
+            th = node;
+        }
+    }
+
+    public int len(ListNode node) {
+        int l = 0;
+        while (node != null) {
+            l++;
+            node = node.next;
+        }
+        return l;
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {       //S.M. ORIGINAL METHOD 
+        if (head == null || head.next == null || k == 0 || k == 1)
+            return head;
+
+        int len = len(head);
+        if (len < k)
+            return head;
+
+        ListNode curr = head;
+        while (curr != null) {
+            int tk = k;
+
+            while (tk-- > 0) {
+                ListNode forw = curr.next;
+
+                curr.next = null;
+                addFirstNode(curr);
+
+                curr = forw;
+            }
+
+            len -= k;
+
+            if (ot == null) {
+                oh = th;
+                ot = tt;
+            } else {
+                ot.next = th;
+                ot = tt;
+            }
+
+            th = null;
+            tt = null;
+
+            if (len < k) {
+                ot.next = curr;
+                curr = null;
+            }
+        }
+        return oh;
+    }
+
+
+    
     public static void display(ListNode node) {
         if(node == null) return;
         System.out.print(node.val + " ");
