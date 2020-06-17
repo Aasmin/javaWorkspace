7d908205ea411c4a9a933f84698543847c8848f9
Aasmin <aasmin.kainth@gmail.com>
2020-06-17 17:39:08 -0400

    ST next Greater Element To The Right

--- Stack/questions.java
+++ Stack/questions.java
@@ -1,4 +1,5 @@
 import java.util.Stack;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class questions {
@@ -42,7 +43,23 @@ public class questions {
         return true;
     }
 
-    
+    public static int[] nextGreaterElementToTheRight(int[] arr){    //O(n)
+        int[] ans = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        st.push(arr[arr.length - 1]);
+        ans[arr.length - 1] = -1;
+        for(int i = arr.length - 2; i >= 0; i--) {  
+            //- a +
+            while(!st.isEmpty() && arr[i] >= st.peek()) //complexity is O(n) as n elements hi push hue and n elements hi pop hue
+                st.pop();
+            
+            if(st.isEmpty())    ans[i] = -1;
+            else    ans[i] = st.peek();
+
+            st.push(arr[i]);
+        }
+        return ans;
+    } 
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
