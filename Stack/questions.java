72f6b71eb0a6f4a04f4ca5ad40e5ff9393f8519c
Aasmin <aasmin.kainth@gmail.com>
2020-06-17 22:24:57 -0400

    ST next Smaller Element To The Right approaches

--- Stack/questions.java
+++ Stack/questions.java
@@ -79,13 +79,58 @@ public class questions {
         
         return ans;
     } 
+    
+    //SAME AS ABOVE JUST CHANGED THE CONDITION FOR POP()
+    public static int[] nextSmallerElementToTheRight(int[] arr){    //O(2n) ~ O(n)  [RIGHT TO LEFT]
+        int[] ans = new int[arr.length];
+        Stack<Integer> st = new Stack<>();  //STACK WILL STORE THE ACTUAL VALUES.
+        st.push(arr[arr.length - 1]);
+        ans[arr.length - 1] = -1;
+        for(int i = arr.length - 2; i >= 0; i--) {  
+            //- a +
+            while(!st.isEmpty() && arr[i] < st.peek()) //complexity is O(n) as n elements hi push hue and n elements hi pop hue
+                st.pop();
+            
+            if(st.isEmpty())    ans[i] = -1;
+            else    ans[i] = st.peek();
+
+            st.push(arr[i]);
+        }
+        return ans;
+    } 
+
+    public static int[] nextSmallerElementToTheRight02(int[] arr){    //O(2n) ~ O(n) [LEFT TO RIGHT]
+        int[] ans = new int[arr.length];
+        Stack<Integer> st = new Stack<>();  //STACK WILL STORE THE INDEXES.
+        for(int i = 0; i < arr.length; i++) {
+            while(!st.isEmpty() && arr[i] < arr[st.peek()]) { 
+                int pos = st.peek();
+                ans[pos] = arr[i];
+                st.pop();
+            }
+            st.push(i); //Store the INDEX.
+        }
+        while(!st.isEmpty()){
+            ans[st.peek()] = -1;
+            st.pop();
+        }
+        
+        return ans;
+    } 
+    
 
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        String str = scn.nextLine();
+        // Scanner scn = new Scanner(System.in);
+        // String str = scn.nextLine();
 
-        System.out.println(balancedBrackets(str));
+        // System.out.println(balancedBrackets(str));
 
+        int arr[] = {2, 5, 9, 3, 1, 12, 6, 8, 7};
+        System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(nextGreaterElementToTheRight(arr)));
+        // System.out.println(Arrays.toString(nextGreaterElementToTheRight02(arr)));
+        System.out.println(Arrays.toString(nextSmallerElementToTheRight(arr)));
+        System.out.println(Arrays.toString(nextSmallerElementToTheRight02(arr)));
         
     }
 }
