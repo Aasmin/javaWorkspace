c0108d2275c2151477058b32d2f1a1164dd69009
Aasmin <aasmin.kainth@gmail.com>
2020-06-19 18:01:09 -0400

    ST find Celebrity

--- Stack/questions.java
+++ Stack/questions.java
@@ -409,6 +409,40 @@ public class questions {
         System.out.println(postfixSt.pop());
     }
 
+    public static void findCelebrity(int[][] arr) {
+        // if a celebrity is there print it''s index (not position), if there is not then
+        // print "none"
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < arr.length; i++)     st.push(i);
+
+        while(st.size() > 1){
+           int i = st.pop();
+           int j = st.pop();
+
+           if(arr[i][j] == 1){
+               st.push(j);
+           } else {
+            st.push(i);
+           }
+        }
+
+        int pot = st.pop();
+        for(int i  = 0; i < arr.length; i++) 
+            if(arr[pot][i] == 1) {
+                System.out.println("none");
+                return;
+            }
+
+        for(int i  = 0; i < arr.length; i++){
+            if(i == pot)   continue;
+            if(arr[i][pot] == 0) {
+                System.out.println("none");
+                return;
+            }
+        }
+        System.out.println(pot);   
+    }
+
     public static void main(String[] args) {
         // Scanner scn = new Scanner(System.in);
         // String str = scn.nextLine();
