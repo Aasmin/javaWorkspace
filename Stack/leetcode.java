1e6766de8cd57904c5b1cea51c0492e115a2bbca
Aasmin <aasmin.kainth@gmail.com>
2020-06-27 18:44:01 -0400

    ST LC 946. Validate Stack Sequences

--- Stack/leetcode.java
+++ Stack/leetcode.java
@@ -449,6 +449,20 @@ public class leetcode {
         return (int) minSF;
     }
     }
+
+    //Leetcode 946. Validate Stack Sequences
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> st = new Stack<>();
+        int i = 0;
+        for(int ele : pushed) {
+            st.push(ele);
+            while(!st.isEmpty() && st.peek() == popped[i]) {
+                st.pop();
+                i++;
+            }
+        }
+        return st.isEmpty();
+    }
      
     public static void main(String[] args) {
         // System.out.println(removeOuterParentheses("(()())(())"));
