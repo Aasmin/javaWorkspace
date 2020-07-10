0eea52cabbad9cdfbf79891d5891248e468febf4
Aasmin <aasmin.kainth@gmail.com>
2020-07-09 21:37:29 -0400

    DP LIS rightToLeft

--- DP/leetcode.java
+++ DP/leetcode.java
@@ -938,11 +938,28 @@ public class leetcode {
         return omax;
     }
 
+    static int LIS_rightToLeft(int[] arr, int[] dp) {
+        int n = arr.length;
+        int omax = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            dp[i] = 1;
+            for(int j = i + 1; j < n; j++) 
+                if(arr[j] > arr[i])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            omax = Math.max(omax, dp[i]);
+        }
+        return omax;
+    }
+
     static void LIS_type() {
         int[] arr = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
         int[] dp = new int[arr.length];
         System.out.println(LIS_leftToRight(arr, dp));
         System.out.println(Arrays.toString(dp));
+        
+        int[] dp2 = new int[arr.length];
+        System.out.println(LIS_rightToLeft(arr, dp2));
+        System.out.println(Arrays.toString(dp2));
     }
 
     static void display2D(int[][] array) {
