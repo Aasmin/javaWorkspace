3d007a377c91e013312f34bb4bfa1683578d9333
Aasmin <aasmin.kainth@gmail.com>
2021-02-07 00:29:21 -0800

    LC 1091

--- Graph/leetcode.java
+++ Graph/leetcode.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 public class leetcode {
     //JO REGION SURROUNDED HAI, usko 'X' mein convert krdo
     //Leetcode 130. Surrounded Regions    
@@ -123,11 +125,60 @@ public class leetcode {
     }
 
 
-    // Count the number of distinct island: 
+    // ND - Count the number of distinct island: 
     // An island consider to be the same as another iff one island can be translated (and not rotated/reflected) to other island.
     
 
-    
+    // Leetcode 1091. Shortest Path in Binary Matrix
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        if (n == 0)
+            return -1;
+        int m = grid[0].length;
+        if (m == 0)
+            return -1;
+
+        if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1)
+            return -1;
+
+        int[][] dirA = {{-1, -1}, {-1, 0}, {0, -1}, {-1, 1}, {1, -1}, {0, 1}, {1, 0}, {1, 1}};
+        LinkedList<Integer> que = new LinkedList<>();
+        que.addLast(0);
+        grid[0][0] = 1;
+
+        int level = 1;
+        while (que.size() != 0)
+        {
+            int size = que.size();
+            while (size-- > 0)
+            {
+                int rvtx = que.removeFirst();
+                
+                int r = rvtx / m;
+                int c = rvtx % m;
+
+                if (r == n - 1 && c == m - 1)
+                    return level;
+
+                for (int d = 0; d < 8; d++)
+                {
+                    int x = r + dirA[d][0];
+                    int y = c + dirA[d][1];
+
+                    if (x >= 0 && x < n && y >= 0 && y < m && grid[x][y] == 0)
+                    {
+                        que.addLast((x * m + y));
+                        grid[x][y] = 1;
+                    }
+                }
+            }
+            level++;
+        }
+
+        return -1;
+    }
+
+
     public static void main(String[] args) {
 
     }
